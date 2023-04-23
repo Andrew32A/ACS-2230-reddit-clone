4bbd376bdf71ba9efe12cb6df748a59727332877
Andrew Alsing <andrew32alsing@gmail.com>
2023-04-23 16:05:17 -0700

    Users can view a list of posts

--- controllers/posts.js
+++ controllers/posts.js
@@ -6,3 +6,12 @@ module.exports = (app) => {
     console.log(req.body);
   });
 };
+
+
+app.get('/', (req, res) => {
+  Post.find({}).lean()
+    .then((posts) => res.render('posts-index', { posts }))
+    .catch((err) => {
+      console.log(err.message);
+    })
+})
