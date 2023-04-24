b29e67c2a093a33fa8c39161224a6598ea15d220
Andrew Alsing <andrew32alsing@gmail.com>
2023-04-23 18:10:56 -0700

    Users can see Subreddits

--- controllers/posts.js
+++ controllers/posts.js
@@ -1,35 +1,43 @@
-const Post = require('../models/post');
+const Post = require("../models/post");
 
 module.exports = (app) => {
   // INDEX
-  app.get('/', (req, res) => {
-    Post.find({}).lean()
-      .then((posts) => res.render('posts-index', { posts }))
-      .catch((err) => {
-        console.log(err.message);
-      })
-  });
-
-  // CREATE
-  app.get('/posts/new', (req, res) => {
-    res.render("posts-new");
-  });
-  
-  app.post('/posts/new', (req, res) => {
-    console.log(req.body);
-    const post = new Post(req.body);
-  
-    post.save()
-      .then(() => res.redirect('/'))
-      .catch(err => console.log(err));
-  });
-  
-  // SHOW
-  app.get('/posts/:id', (req, res) => {
-    Post.findById(req.params.id).lean()
-      .then((post) => res.render('posts-show', { post }))
+  app.get("/", (req, res) => {
+    Post.find({})
+      .lean()
+      .then((posts) => res.render("posts-index", { posts }))
       .catch((err) => {
         console.log(err.message);
       });
+  });
+
+  // CREATE
+  app.get("/posts/new", (req, res) => {
+    res.render("posts-new");
+  });
+
+  app.post("/posts/new", (req, res) => {
+    console.log(req.body);
+    const post = new Post(req.body);
+
+    post
+      .save()
+      .then(() => res.redirect("/"))
+      .catch((err) => console.log(err));
+  });
+
+  // SHOW
+  app.get("/posts/:id", (req, res) => {
+    Post.findById(req.params.id)
+      .lean()
+      .then((post) => res.render("posts-show", { post }))
+      .catch((err) => {
+        console.log(err.message);
+      });
+  });
+
+  // SUBREDDIT
+  app.get("/n/:subreddit", (req, res) => {
+    console.log(req.params.subreddit);
   });
 };
