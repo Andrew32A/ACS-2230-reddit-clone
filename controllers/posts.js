be2b3abfb627aed191ccac641d6d952708b2c833
Andrew Alsing <andrew32alsing@gmail.com>
2023-04-29 20:17:21 -0700

    added author's name to posts

--- controllers/posts.js
+++ controllers/posts.js
@@ -1,15 +1,19 @@
 const Post = require("../models/post");
+const User = require("../models/user");
+const Comment = require("../models/comment");
 
 module.exports = (app) => {
   // INDEX
-  app.get("/", async (req, res) => {
-    const currentUser = req.user;
-    try {
-      const posts = await Post.find({}).lean();
-      return res.render("posts-index", { posts, currentUser });
-    } catch (err) {
-      console.log(err.message);
-    }
+  app.get("/", (req, res) => {
+    const { user } = req;
+    console.log(req.cookies);
+    Post.find({})
+      .lean()
+      .populate("author")
+      .then((posts) => res.render("posts-index", { posts, user }))
+      .catch((err) => {
+        console.log(err.message);
+      });
   });
 
   // CREATE
@@ -17,17 +21,27 @@ module.exports = (app) => {
     if (req.user) {
       res.render("posts-new");
     } else {
-      res.redirect("/")
+      res.redirect("/");
     }
   });
 
   app.post("/posts/new", (req, res) => {
     if (req.user) {
+      const userId = req.user._id;
       const post = new Post(req.body);
+      post.author = userId;
       post
         .save()
-        .then(() => res.redirect("/"))
-        .catch((err) => console.log(err));
+        .then(() => User.findById(userId))
+        .then((user) => {
+          user.posts.unshift(post);
+          user.save();
+          // REDIRECT TO THE NEW POST
+          return res.redirect(`/posts/${post._id}`);
+        })
+        .catch((err) => {
+          console.log(err.message);
+        });
     } else {
       return res.status(401); // UNAUTHORIZED
     }
